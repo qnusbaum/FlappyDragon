2995a4b44397f698cd86a17b36c242ea6758ea60
Antonin <arisser>
2018-03-27 10:58:34 +0200

    Dragon qui rentre en hyper espace.

--- app/src/main/java/network/iut/org/flappydragon/Background.java
+++ app/src/main/java/network/iut/org/flappydragon/Background.java
@@ -11,6 +11,7 @@ public class Background {
     private GameView view;
     private Bitmap background1;
     private Bitmap background4;
+    private int xTranslation;
 
     public Background(Context context, GameView view) {
         height = context.getResources().getDisplayMetrics().heightPixels;
@@ -18,17 +19,22 @@ public class Background {
         background1 = Util.decodeSampledBitmapFromResource(context.getResources(), R.drawable.layer1, width, height);
         background4 = Util.decodeSampledBitmapFromResource(context.getResources(), R.drawable.layer4, width, height);
         this.view = view;
+        xTranslation=0;
     }
 
     public void draw(Canvas canvas) {
          canvas.drawBitmap(background1,
-                 new Rect(0, 0, background1.getWidth()/2, background1.getHeight()),
+                 new Rect(xTranslation, 0, background1.getWidth(), background1.getHeight()),
                  new Rect(0, 0, background1.getWidth(), height),
                  null);
-         canvas.drawBitmap(background1,
-                 new Rect(0, 0, background1.getWidth()/2, background1.getHeight()),
-                 new Rect(background1.getWidth(), 0, background1.getWidth(), height),
-                 null);
+         xTranslation +=10;
+        canvas.drawBitmap(background1,
+                new Rect(background1.getWidth()+xTranslation, 0, background1.getWidth(), background1.getHeight()),
+                new Rect(0, 0, background1.getWidth(), height),
+                null);
+         if( xTranslation >= background1.getWidth()){
+             xTranslation=0;
+         }
          canvas.drawBitmap(background4, new Rect(0, 0, background1.getWidth(), background1.getHeight()), new Rect(0, 0, width, height), null);
     }
 }
