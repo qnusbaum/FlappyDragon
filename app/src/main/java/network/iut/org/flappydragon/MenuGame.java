e8d7dac7cf4aefdacd9cb97fbe00d2e912eb96c6
Quentin <quentin.nusb@gmail.com>
2018-04-12 11:05:01 +0200

    init difficultées

--- app/src/main/java/network/iut/org/flappydragon/MenuGame.java
+++ app/src/main/java/network/iut/org/flappydragon/MenuGame.java
@@ -1,9 +1,13 @@
 package network.iut.org.flappydragon;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 /**
@@ -25,19 +29,30 @@ public class MenuGame extends Activity {
     public void initOption() {
         setContentView(R.layout.options);
         Button retour = (Button) findViewById(R.id.btnRetour);
+
         retour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+                int radioButtonID = radioGroup.getCheckedRadioButtonId();
+                RadioButton radioButtonChecked = (RadioButton) radioGroup.findViewById(radioButtonID);
+                String selectedtext = (String) radioButtonChecked.getText();
+                Log.e(selectedtext,selectedtext);
                 initMenu();
             }
         });
-        //RadioGroup
+
     }
 
     public void initMenu() {
         setContentView(R.layout.menu_game);
         Button jouer = (Button) findViewById(R.id.btnJouer);
         Button option = (Button) findViewById(R.id.btnOptions);
+
+        SharedPreferences sharedPref = .getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(getString(R.string.saved_high_score_key), newHighScore);
+        editor.commit();
 
         jouer.setOnClickListener(new View.OnClickListener() {
             @Override
