026d5cc48711bb1c8455cc6edadb25fb16fda751
Antonin <antonin.risser@gmail.com>
2018-04-16 20:03:36 +0200

    Modale box pour restart

--- app/src/main/java/network/iut/org/flappydragon/Background.java
+++ app/src/main/java/network/iut/org/flappydragon/Background.java
@@ -27,16 +27,20 @@ public class Background {
         xTranslation=0;
     }
 
+    /**
+     * Used to draw background of the application
+     * @param  canvas The canvas
+     */
     public void draw(Canvas canvas) {
-        //Gestion du ciel
-         canvas.drawBitmap(background1,
-                 new Rect(0, 0, background1.getWidth(), background1.getHeight()),
-                 new Rect(+xTranslation, 0, background1.getWidth()+xTranslation, height),
-                 null);
-         xTranslation -=1;
-         canvas.drawBitmap(background1,
+        //TODO : Correct bug
+        canvas.drawBitmap(background1,
                 new Rect(0, 0, background1.getWidth(), background1.getHeight()),
-                new Rect(background1.getWidth()+xTranslation, 0, background1.getWidth()*2+xTranslation, height),
+                new Rect(+xTranslation, 0, width * background1.getWidth() /  background1.getHeight()+xTranslation, height),
+                null);
+        xTranslation -=1;
+        canvas.drawBitmap(background1,
+                new Rect(0, 0, background1.getWidth(), background1.getHeight()),
+                new Rect(background1.getWidth()+xTranslation, 0, (width * background1.getWidth() /  background1.getHeight())*2+xTranslation, height),
                 null);
          if( xTranslation <= -background1.getWidth()){
              xTranslation=0;
@@ -45,12 +49,12 @@ public class Background {
          //Gestion des arbres
         canvas.drawBitmap(background4,
                 new Rect(0, 0, background1.getWidth(), background1.getHeight()),
-                new Rect(+xTranslationArbre, 0, width * background1.getWidth() /  background1.getHeight()+xTranslationArbre, height),
+                new Rect(+xTranslationArbre, 0, width * background4.getWidth() /  background4.getHeight()+xTranslationArbre, height),
                 null);
         xTranslationArbre -=10;
         canvas.drawBitmap(background4,
                 new Rect(0, 0, background1.getWidth(), background1.getHeight()),
-                new Rect(background1.getWidth()+xTranslationArbre, 0, (width * background1.getWidth() /  background1.getHeight())*2+xTranslationArbre, height),
+                new Rect(background4.getWidth()+xTranslationArbre, 0, (width * background4.getWidth() /  background4.getHeight())*2+xTranslationArbre, height),
                 null);
         if( xTranslationArbre <= -background1.getWidth()){
             xTranslationArbre=0;
@@ -59,7 +63,7 @@ public class Background {
         //Gestion du sol
         canvas.drawBitmap(background5,
                 new Rect(0, 0, background5.getWidth(), background5.getHeight()),
-                new Rect(+xTranslationSol, 0, width * background5.getWidth() /  background5.getHeight()+xTranslationSol, height),
+                new Rect(+xTranslationSol, 0, (width * background5.getWidth() /  background5.getHeight())+xTranslationSol, height),
                 null);
         xTranslationSol -=10;
         canvas.drawBitmap(background5,
